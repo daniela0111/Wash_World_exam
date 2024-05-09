ac6cf3816f49e36ae2420b5c45b8094e98c23f26
daniela0111 <95025799+daniela0111@users.noreply.github.com>
2024-05-09 22:03:17 +0200

    .env

---
+++ ormConfig.ts
@@ -0,0 +1,21 @@
+//This file defines the connection details for database
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+export const ormConfig = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT) || 5434,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['src/**/*.entity.{ts,js}'],
+};
+
+// Inject TypeORMModule with ormConfig
+@Module({
+  imports: [TypeOrmModule.forRoot(ormConfig)],
+  // ...
+})
+export class AppModule {}
+
