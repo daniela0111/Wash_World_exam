6b00291fe80eb397d3d2b36c4c56aa71f7c29d64
daniela0111 <95025799+daniela0111@users.noreply.github.com>
2024-05-20 21:43:54 +0200

    remove scripts

--- Wash_World/App.tsx
+++ Wash_World/App.tsx
@@ -13,8 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    textDecorationColor: '#ffffff',
+  }
 });
