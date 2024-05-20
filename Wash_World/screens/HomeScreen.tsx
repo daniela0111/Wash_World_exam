0088816d3c88181fcee10a2f7cc6f90e0904bfee
daniela0111 <95025799+daniela0111@users.noreply.github.com>
2024-05-20 20:53:24 +0200

    background color

---
+++ Wash_World/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View} from 'react-native';
+
+const BackgrountComponent = () => {
+    return (
+       <View style={{backgroundColor: '#000000' }}> 
+       </View>
+    );
+};
+
+export default BackgrountComponent;
+
+
